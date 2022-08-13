15e60d75d6272afdcf5a8857fc048269afce7949
Lezier <lezier.f_@hotmail.com>
2022-08-12 23:48:15 -0300

    Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { ThemeSwitcher } from "../../hooks/ThemeSwitcher";
-import { Sty } from "./styles";
+import * as Styled from "./styles";
 import Pokemon from "../../assets/pngs/pokemon.png";
+import Pokeblu from "../../assets/pngs/pokeblu.png";
 
 export const Header: React.FC = () => {
   return (
-    <Sty.Container>
-      <Sty.Divison>
+    <Styled.Container>
+      <Styled.HeaderContent>
         <div className="image">
           <img src={Pokemon} alt="pokemon" />
+        </div>
+
+        <div className="pokeblu">
+          <img src={Pokeblu} alt='pokebola azul'  />
         </div>
 
         <div className="theme">
           <ThemeSwitcher />
         </div>
-      </Sty.Divison>
-    </Sty.Container>
+
+
+      </Styled.HeaderContent>
+    </Styled.Container>
   );
 };
