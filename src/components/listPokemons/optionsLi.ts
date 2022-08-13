4cc595f921130afcc839fda6127c0bcbcfefa54a
Lezier <lezier.f_@hotmail.com>
2022-08-12 21:56:12 -0300

    options para lista

---
+++ src/components/listPokemons/optionsLi.ts
@@ -0,0 +1,98 @@
+import Bug from "../../assets/icons/bug"
+import Dark from "../../assets/icons/dark"
+import Dragon from "../../assets/icons/dragon"
+import Normal from "../../assets/icons/Normal"
+import Electric from "../../assets/icons/electric"
+import Fairy from "../../assets/icons/Fairy"
+import Fighting from "../../assets/icons/Fighting"
+import Fire from "../../assets/icons/Fire"
+import Flying from "../../assets/icons/Flying"
+import Ghost from "../../assets/icons/Ghost"
+import Grass from "../../assets/icons/Grass"
+import Ground from "../../assets/icons/Ground"
+import Ice from "../../assets/icons/Ice"
+import Poison from "../../assets/icons/Poison"
+import Psychic from "../../assets/icons/Psychic"
+import Rock from "../../assets/icons/Rock"
+import Steel from "../../assets/icons/Steel"
+import Water from "../../assets/icons/Water"
+import AllPokemons from "../../assets/icons/All"
+
+export const ListType = [
+    {
+        type: "All",
+        iconType: AllPokemons,
+    },
+    {
+        type: "Normal",
+        iconType: Normal,
+    },
+    {
+        type: "Fighting",
+        iconType: Fighting,
+    },
+    {
+        type: "Flying",
+        iconType: Flying,
+    },
+    {
+        type: "Poison",
+        iconType: Poison,
+    },
+    {
+        type: "Ground",
+        iconType: Ground,
+    },
+    {
+        type: "Rock",
+        iconType: Rock,
+    },
+    {
+        type: "Bug",
+        iconType: Bug,
+    },
+    {
+        type: "Ghost",
+        iconType: Ghost,
+    },
+    {
+        type: "Steel",
+        iconType: Steel,
+    },
+    {
+        type: "Fire",
+        iconType: Fire,
+    },
+    {
+        type: "Water",
+        iconType: Water,
+    },
+    {
+        type: "Grass",
+        iconType: Grass,
+    },
+    {
+        type: "Electric",
+        iconType: Electric,
+    },
+    {
+        type: "Psychic",
+        iconType: Psychic,
+    },
+    {
+        type: "Ice",
+        iconType: Ice,
+    },
+    {
+        type: "Dragon",
+        iconType: Dragon,
+    },
+    {
+        type: "Dark",
+        iconType: Dark,
+    },
+    {
+        type: "Fairy",
+        iconType: Fairy,
+    },
+]
