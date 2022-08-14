14046c440cee4df413a52f11d0fe8257c4885bab
Lezier <lezier.f_@hotmail.com>
2022-08-14 16:44:57 -0300

    ajustes de layout

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Container = styled.div`
   min-height: 100vh;
 
-  background-color: ${({ theme }) => theme.colors.primary.solid};
+  background-color: ${({ theme }) => theme.colors.card};
 `;
--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -7,7 +7,8 @@ export const defaultTheme = {
       solid: "#FFF",
     },
     card: "#f7f7f7",
-    font: "#403D3D",
+    background: "#FFF",
+    font: "#7A7D80",
     input: {
       text: "#2873B9",
       bg: "#fff"
@@ -25,6 +26,7 @@ export const darkTheme = {
           solid: "#FFF",
         },
         card: "#403D3D",
+        background: "#181A1B",
         font: "#fff",
         input: {
           text: "#FFCC01",
