9486fe6f7c1042bccd7a2c092d881dd47b31ca62
Lezier <lezier.f_@hotmail.com>
2022-08-13 21:28:16 -0300

    ajustes no list pokemons

--- src/components/listPokemons/index.tsx
+++ src/components/listPokemons/index.tsx
@@ -6,11 +6,13 @@ import { useState } from "react";
 import { PaginationComponent } from "../Pagination";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import noImage from "../../assets/pngs/NoImage.png";
-import Bug from "../../assets/svgs/types/Bug";
+import Bug from '../../assets/svgs/types/bug'
+import { typesList } from "./typesList";
+import Pokebola from "../../assets/svgs/types/pokebola";
 
 export const ListPokemons = () => {
-  const { allPokemonsList, setSearch, search, filtered, count } = useApiContext();
-  console.log(allPokemonsList)
+
+  const { allPokemonsList, setSearch, search, filtered, count, setType, type } = useApiContext();
 
   const [ref] = useKeenSlider<HTMLDivElement>();
 
@@ -29,20 +31,44 @@ export const ListPokemons = () => {
           <input
             type="search"
             value={search}
+            placeholder='Pesquise um pokémon'
             onChange={(event) => setSearch(event.target.value)}
           />
         </div>
       </Styled.Filter>
       <Styled.Contents>
         <Styled.LeftContent>
-
+          <Styled.UlList>
+            {
+              typesList.map((item: any) => {
+                return (
+                  <>
+                    <Styled.LiList
+                      onClick={() => {
+                        setType(item.key)
+                      }}
+                      type={type}
+                      keyAtual={item.key}
+                    >
+                      {<item.iconType width={24} key={item.key} />}
+                      <span >{item.type}</span>
+                    </Styled.LiList>
+                  </>
+                )
+              })
+            }
+          </Styled.UlList>
         </Styled.LeftContent>
 
+        <Styled.DividerMenu />
+
         <Styled.RightContainer>
-          
+
           <Styled.TitleCount>
-            <Bug width={42} height={42} fill="#111"/>
-            <span>{(count * 10)} Pokémons</span>
+            <Pokebola width={28} height={28} />
+            {
+              type === 0 ? (<span>{count * 10} Pokémons</span>) : (<span>{count} Pokémons</span>)
+            }
           </Styled.TitleCount>
 
           <Styled.RightContent>
@@ -50,7 +76,6 @@ export const ListPokemons = () => {
             {
               search.length > 0 ? (
                 filtered.map((item: any, key: any) => {
-                  console.log('filtrado')
                   return (
                     <Styled.ContainerList key={key}>
                       <Styled.Div>
@@ -97,60 +122,35 @@ export const ListPokemons = () => {
                 })
               ) : (
                 allPokemonsList.map((item: any, key: any) => {
-                  console.log('total')
+
+                  console.log(item.sprites.other.dream_world.front_default)
                   return (
-                    <Styled.ContainerList key={key}>
-                      <Styled.Div>
-                        <div ref={ref} className="keen-slider">
-                          <div className="keen-slider__slide number-slide1" id="slide">
-                            {!!item.sprites.front_default ? (
-                              <img src={item.sprites.front_default} alt="pokemonPics" />
-                            ) : (
-                              <img src={noImage} alt="noimagefound"></img>
-                            )}
-                          </div>
-
-                          <div className="keen-slider__slide number-slide1" id="slide">
-                            <img src={item.sprites.front_shiny} alt="pokemonPics" />
-                          </div>
-                          <div className="keen-slider__slide number-slide1" id="slide">
-                            <img src={item.sprites.back_default} alt="pokemonPics" />
-                          </div>
-                          <div className="keen-slider__slide number-slide1" id="slide">
-                            <img src={item.sprites.back_shiny} alt="pokemonPics" />
-                          </div>
-                        </div>
-                      </Styled.Div>
-                      <a href={`/${item.name}`} rel="noreferrer noopener">
+                    <a href={`/${item.name}`} rel="noreferrer noopener">
+                      <Styled.ContainerList key={key}>
+                        <Styled.Div>
+                            <Styled.ImagePokemon src={item.sprites.other.dream_world.front_default}/>
+                        </Styled.Div>
                         <Styled.DivTwo>
-                          <span className="name"> {item.name}</span>
 
-                          <span>
-                            {"#"}
-                            {item.id}
-                          </span>
-                          <div id="sizes">
-                            <span>Peso: {item.weight}kg</span>
-                            <span> Altura: {item.height}m</span>
-                          </div>
-                          <div id="types">
-                            <span> {item.types[0].type.name}</span>
-                            {item.types[1] && <span> {item.types[1].type.name}</span>}
-                          </div>
                         </Styled.DivTwo>
-                      </a>
-                    </Styled.ContainerList>
+                      </Styled.ContainerList>
+                    </a>
                   );
                 })
 
               )
             }
           </Styled.RightContent>
+          {
+            type === 0 && (
+              <Styled.Pag>
+                <PaginationComponent />
+              </Styled.Pag>
+            )
+          }
         </Styled.RightContainer>
       </Styled.Contents>
-      <Styled.Pag>
-        <PaginationComponent />
-      </Styled.Pag>
+
     </Styled.Container>
   );
 };
--- src/components/listPokemons/styles.ts
+++ src/components/listPokemons/styles.ts
@@ -8,19 +8,25 @@ export const Container = styled.div`
 export const Contents = styled.div`
   display: flex;
   background: #FFF;
-  box-shadow: 0px 5px 5px 4px rgba(0,0,0,0.56);
-  -webkit-box-shadow: 0px 5px 5px 4px rgba(0,0,0,0.56);
-  -moz-box-shadow: 0px 5px 5px 4px rgba(0,0,0,0.56);
 
   @media (min-width: 472px) {
     
   }
 `;
 
+export const DividerMenu = styled.div`
+  border-right: 1px solid rgba(0,0,0,0.10);
+  margin-inline: 25px;
+`;
+
 export const LeftContent = styled.div`
   width: 100%;
   max-width: 22rem;
   min-width: 240px;
+  display: flex;
+  justify-content: right;
+  margin-top: 68px;
+
 `;
 
 export const RightContent = styled.div`
@@ -40,11 +46,11 @@ export const RightContent = styled.div`
     width: 100%;
     grid-template-columns: repeat(3, 1fr);
   }
-  @media (max-width: 968px) {
+  @media (max-width: 1040px) {
     width: 100%;
     grid-template-columns: repeat(2, 1fr);
   }
-  @media (max-width: 714px) {
+  @media (max-width: 778px) {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
@@ -55,22 +61,17 @@ export const RightContainer = styled.div`
 `;
 
 export const TitleCount = styled.div`
-  margin-block: 55px;
+  margin-block: 65px;
+  display: flex;
+  align-items: center;
   
-  svg{
-    width: 40px;
-    height: 40px;
-
-    path{
-      width: 40px;
-    }
-  }
 
   span{
     font-weight: 600;
+    margin-left: 10px;
     font-size: 1.2rem;
     line-height: 150%;
-    color: #4D5053;
+    color: #6e757b;
     font-family: "Montserrat", sans-serif;
   }
 `;
@@ -83,27 +84,112 @@ export const ContainerList = styled.div`
   height: 220px;
   position: relative;
   margin: 0 auto;
+  display: flex;
+  justify-content:  center;
 
    > a {
     text-decoration: none;
    }
 `;
 
-export const Div = styled.div`
-  width: 100%;
 
-  #slide {
+
+//FILTRO
+
+export const Filter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 74px;
+  margin-block: 52px;
+
+  h4{
+    font-family: "Montserrat", sans-serif;
+    font-weight: bold;
+    font-size: 1.4rem;
+    color: #6e757b;
+  }
+
+  @media (max-width: 600px) {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  div {
     display: flex;
     justify-content: center;
-
-    > img {
-      width: 90px;
-      transition: 0.5s ease-in-out;
-
-      &:hover {
-        transform: translateY(-10px);
-      }
+    
+    .search {
+      left: 35px;
+      top: 8px;
+      position: relative;
     }
+    > input {
+      width: 280px;
+      border: 1px solid white;
+      border-radius: 15px;
+      padding: 10px 40px;
+      outline-color: rgba(0,0,0,0.20);
+      background-color: ${({ theme }) => theme.colors.input.bg};
+    }
+  }
+`;
+
+export const Pag = styled.div`
+  margin-top: 32px;
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+`;
+
+//LISTA DE SELECT LATERAL ESQUERDO
+
+export const UlList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+type PropsLi ={
+  type: any;
+  keyAtual: any
+}
+
+export const LiList = styled.li<PropsLi>`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  min-width: 168px;
+  cursor: pointer;
+  height: 32px;
+  padding: 10px;
+  border-radius: 10px;
+  background: ${props => props.keyAtual === props.type ? "#e1e3e5" : ''};
+
+  span{
+    color: #aaa7a7;
+    box-sizing: border-box;
+    list-style: none;
+    font-family: "Inter", sans-serif;
+  }
+`;
+
+
+//CARDS pokemon 
+
+export const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  background: white;
+  border-radius: 100%;
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+  position: absolute;
+
+
+  img{
+    max-width: 100px;
   }
 `;
 
@@ -153,47 +239,6 @@ export const DivTwo = styled.div`
   }
 `;
 
-export const Filter = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px 74px;
-  margin-block: 52px;
-
-  h4{
-    font-family: "Montserrat", sans-serif;
-    font-weight: bold;
-    font-size: 1.4rem;
-    color: #2F3133;
-  }
-
-  @media (max-width: 600px) {
-    width: 90%;
-    margin: 0 auto;
-  }
-
-  div {
-    display: flex;
-    justify-content: center;
-    
-    .search {
-      left: 35px;
-      top: 8px;
-      position: relative;
-    }
-    > input {
-      width: 280px;
-      border: 1px solid white;
-      border-radius: 15px;
-      padding: 10px 40px;
-      outline-color: rgba(0,0,0,0.20);
-      background-color: ${({ theme }) => theme.colors.input.bg};
-    }
-  }
-`;
-
-export const Pag = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 20px 0;
+export const ImagePokemon = styled.img`
+  
 `;
