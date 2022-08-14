6691334ede09720fda02cd80c5688ee6f269f1d8
Lezier <lezier.f_@hotmail.com>
2022-08-14 03:01:44 -0300

    helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,12 @@
+
+
+export const format3Numbers = (num: number) => {
+    if(!num) return
+    if((num).toString().length < 2) return `00${num}`
+    if((num).toString().length < 3) return `0${num}`
+    return num
+}
+
+export const firstCharToUP = (value: string) => {
+    return value.charAt(0).toLocaleUpperCase() + value.slice(1);
+}
