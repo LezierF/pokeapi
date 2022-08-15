6a1a4e75a8aa0535bdcb1ee5f32ef9901db69f60
Lezier <lezier.f_@hotmail.com>
2022-08-15 16:42:32 -0300

    botao voltar

--- src/assets/svgs/buttonPrev.tsx
+++ src/assets/svgs/buttonPrev.tsx
@@ -3,15 +3,15 @@ import { SVGProps } from "react"
 
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width={24}
-    height={24}
+    width={props.width || 24}
+    height={props.height || 24}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <path
       d="M15.41 16.59 10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41Z"
-      fill="#fff"
+      fill={props.color || "#FFF"}
     />
   </svg>
 )
