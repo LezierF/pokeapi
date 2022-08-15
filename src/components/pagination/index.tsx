8cb2408ef9528730d16580d4517502bebd9c240e
Lezier <lezier.f_@hotmail.com>
2022-08-15 14:05:34 -0300

    teste

---
+++ src/components/pagination/index.tsx
@@ -0,0 +1,27 @@
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { useEffect } from "react";
+import { useApiContext } from "../../contexts/ApiContext";
+import { useTheme } from "../../contexts/useTheme";
+
+const PaginationComponent = () => {
+  const { setCurrPage, count } = useApiContext();
+
+  const handleCurrPage = (_: any, page: number) => {
+    setCurrPage(page);
+  }
+
+  const { isDarkMode } = useTheme()
+
+  useEffect(() => {
+    setCurrPage(prev => prev === 0 ? 1 : prev);
+  }, [])
+
+  return (
+    <Stack spacing={2}>
+      <Pagination count={count} color={isDarkMode ? "secondary" : "primary" } onChange={handleCurrPage}/>
+    </Stack>
+  );
+};
+
+export default PaginationComponent
